e5cc441b5ec049f1f581725565d4d2fde6271b0b
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-25 20:59:25 +0530

    done

--- apps/frontend/src/components/task/TaskCard.tsx
+++ apps/frontend/src/components/task/TaskCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { Button } from "@workspace/ui/components/button";
 import {
   Card,
   CardDescription,
@@ -10,9 +8,7 @@ import {
 import { Clock } from "lucide-react";
 import { Task } from "../../types/types";
 import TaskCardMoreButtons from "./TaskCardMoreButtons";
-import { toast } from "sonner";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import TaskCompletedButton from "./TaskCompletedButton";
 
 // Task card props
 interface TaskCardProps {
@@ -21,44 +17,6 @@ interface TaskCardProps {
 
 // Task card component
 export function TaskCard({ task }: TaskCardProps) {
-  const { data: session } = useSession();
-
-  const router = useRouter();
-
-  // Function to complete task
-  const completeTask = async (task: Task) => {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_HTTP_BACKEND_URL}/task/update-task/${task.id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${session?.user.accessToken}`,
-          },
-          body: JSON.stringify({ completed: !task.completed }),
-        }
-      );
-      if (res.ok) {
-        toast.success("Task completed.", {
-          style: {
-            backgroundColor: "#71f871",
-            color: "#1f2937",
-          },
-        });
-        router.refresh(); // refresh the router
-      }
-    } catch (error) {
-      toast.error("An error occurred. Please try again later", {
-        style: {
-          backgroundColor: "#f87171",
-          color: "#1f2937",
-        },
-      });
-      console.error(error);
-    }
-  };
-
   return (
     <Card className={`${task.completed ? "opacity-60" : ""} group/task`}>
       <CardHeader className="pb-2">
@@ -79,14 +37,7 @@ export function TaskCard({ task }: TaskCardProps) {
           <Clock className="mr-1 h-3 w-3" />
           {new Date(task.createdAt).toISOString().split("T")[0]}
         </div>
-        <Button
-          size="sm"
-          onClick={() => completeTask(task)}
-          className="cursor-pointer"
-          disabled={task.completed}
-        >
-          {task.completed ? "Completed" : "Mark as Done"}
-        </Button>
+        <TaskCompletedButton task={task} />
       </CardFooter>
     </Card>
   );
---
+++ apps/frontend/src/components/task/TaskCompletedButton.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { Task } from "@/src/types/types";
+import { Button } from "@workspace/ui/components/button";
+import { toast } from "sonner";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+
+export default function TaskCompletedButton({ task }: { task: Task }) {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const [isPending, setIsPending] = useState(false);
+
+  // Function to complete task
+  const completeTask = async (task: Task) => {
+    try {
+      setIsPending(true);
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_HTTP_BACKEND_URL}/task/update-task/${task.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user.accessToken}`,
+          },
+          body: JSON.stringify({ completed: !task.completed }),
+        }
+      );
+      if (res.ok) {
+        setIsPending(false);
+        toast.success("Task completed.", {
+          style: {
+            backgroundColor: "#71f871",
+            color: "#1f2937",
+          },
+        });
+        router.refresh(); // refresh the router
+      }
+    } catch (error) {
+      setIsPending(false);
+      toast.error("An error occurred. Please try again later", {
+        style: {
+          backgroundColor: "#f87171",
+          color: "#1f2937",
+        },
+      });
+      console.error(error);
+    }
+  };
+  return (
+    <Button
+      size="sm"
+      onClick={() => completeTask(task)}
+      className="cursor-pointer"
+      disabled={task.completed || isPending}
+    >
+      {task.completed ? (
+        "Completed"
+      ) : isPending ? (
+        <>
+          <Loader2 className="h-4 w-4 animate-spin mr-2" />
+          Processing...
+        </>
+      ) : (
+        "Complete"
+      )}
+    </Button>
+  );
+}
