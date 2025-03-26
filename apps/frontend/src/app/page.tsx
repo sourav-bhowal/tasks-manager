0cf6d088a7d2631bb05a331b5846aeae6dfb3f63
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-26 15:34:26 +0530

    ci cd

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
 export default function Home() {
   // Render the page
-  return <div className="flex min-h-screen w-full flex-col bg-muted/40"></div>;
+  return <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-4xl font-bold text-primary">Welcome to the Task Manager</h1>
+    </div >
+  </div>;
 }
